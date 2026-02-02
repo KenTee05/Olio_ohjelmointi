bf5cdd0b1fe2aedc2bcf072cd958cd4b59a9f6c9
KenTee05 <kenttateemu05@gmail.com>
2026-02-02 10:52:25 +0200

    test

---
+++ Luokka-Harjoitus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class Program
+
+namespace Luokka_Harjoitus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static void Main()
+        {
+            Library library = new Library();
+
+            library.AddBook(new Book("Clean Code", "Robert C. Martin"));
+            library.AddBook(new Book("1984", "George Orwell"));
+
+            library.SaveToFile("books.txt");
+            library.LoadFromFile("books.txt");
+        }
+
+    }
