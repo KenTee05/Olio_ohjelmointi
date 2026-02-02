84132612841101864e3bc8f84f1d4d5f7e5f9f1d
KenTee05 <kenttateemu05@gmail.com>
2026-02-02 10:35:20 +0200

    Testi

---
+++ Luokka_harjoitus/Luokka_harjoitus/Book.cs
@@ -0,0 +1,18 @@
+﻿public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public Book() { }
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title};{Author}";
+    }
+}
---
+++ Luokka_harjoitus/Luokka_harjoitus/Library.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+public class Library
+{
+    private List<Book> books = new List<Book>();
+
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+    }
+
+    public List<Book> GetBooks()
+    {
+        return books;
+    }
+
+    // TALLENNUS TIEDOSTOON
+    public void SaveToFile(string filePath)
+    {
+        List<string> lines = new List<string>();
+
+        foreach (Book book in books)
+        {
+            lines.Add(book.ToString());
+        }
+
+        File.WriteAllLines(filePath, lines);
+    }
+
+    // LUKU TIEDOSTOSTA
+    public void LoadFromFile(string filePath)
+    {
+        books.Clear();
+
+        if (!File.Exists(filePath))
+            return;
+
+        string[] lines = File.ReadAllLines(filePath);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(';');
+            books.Add(new Book(parts[0], parts[1]));
+        }
+    }
+}
